a456ae9f9a392d6719ac5ea42659c251d4f3ca59
Thanh Dang <thanh.dang0811@gmail.com>
2023-10-18 21:00:15 -0400

    Changed indexing to be name based instead of looking at specific column

--- twelvedata.py
+++ twelvedata.py
@@ -9,10 +9,10 @@ from constants import apikey
 apikey = apikey
 website = "https://api.twelvedata.com"
 request_type = "/time_series?"
-parameters  = {"symbol": "msft",
+parameters  = {"symbol": "aapl",
                "interval": "1day",
                "apikey": apikey,
-               "outputsize":"100"} # Size can be 1-5000
+               "outputsize":"1000"} # Size can be 1-5000
 
 request = website + request_type
 
@@ -21,20 +21,20 @@ request = website + request_type
 def create_labels(dataframe: pd.DataFrame):
     labels = []
     index_count = 0
-    previous_row = 0
-    for current_row in dataframe.itertuples():
+    prev_close = 0
+    for current_close in dataframe['close'].to_numpy():
         if index_count > 0:
             # define label according to price change
             # If previous day is almost same as current day (disregard decimals), 
             #       set label as 0
             # If previous day is lower than current day, set label as 1
             # If previous day is higher than current day, set label as -1
-            if (float(previous_row[2]) == float(current_row[2])): labels.insert(0,0)
-            elif (float(previous_row[2]) < float(current_row[2])): labels.append(1)
-            elif (float(previous_row[2]) > float(current_row[2])): labels.append(-1)
+            if (prev_close == current_close): labels.insert(0,0)
+            elif (prev_close < current_close): labels.append(1)
+            elif (prev_close > current_close): labels.append(-1)
         else: labels.append(0)
         index_count = index_count + 1
-        previous_row = current_row
+        prev_close = current_close
     dataframe['label'] = labels
     return dataframe
 
@@ -46,9 +46,9 @@ def calculate_simple_moving_average(dataframe:pd.DataFrame):
     index_count = 0
     sma_buffer = []
     std_dev_buffer = []
-    for current_row in dataframe.itertuples():
+    for close in dataframe['close'].to_numpy():
         if index_count > 19:
-            n_days_price_buffer.insert(0,float(current_row[2]))
+            n_days_price_buffer.insert(0,close)
             n_days_price_buffer.pop()
             avg = sum(n_days_price_buffer)/N_DAYS
             variance = 0
@@ -65,7 +65,7 @@ def calculate_simple_moving_average(dataframe:pd.DataFrame):
             index_count = index_count + 1
         else:
             index_count = index_count + 1
-            n_days_price_buffer.insert(0,float(current_row[2]))
+            n_days_price_buffer.insert(0,close)
             sma_buffer.append(0)
             std_dev_buffer.append(0)
     return sma_buffer,std_dev_buffer
@@ -121,34 +121,33 @@ def calculate_obv(dataframe: pd.DataFrame):
         obv.append(current_balance/volume)
     dataframe['obv_norm'] = obv
     return dataframe
-
+    
 
 def main():
     
     # GET JSON FROM REST API
     # response = requests.get(url=request, params=parameters)
     # obj = json.loads(response.content)
-
     # USE JSON MOCK DATA
     # json_dump = '{"meta": {"symbol": "AAPL", "interval": "1day", "currency": "USD", "exchange_timezone": "America/New_York", "exchange": "NASDAQ", "mic_code": "XNGS", "type": "Common Stock"}, "values": [{"datetime": "2023-10-13", "open": "181.42000", "high": "181.92999", "low": "178.14000", "close": "178.85001", "volume": "51427100"}, {"datetime": "2023-10-12", "open": "180.07001", "high": "182.34000", "low": "179.03999", "close": "180.71001", "volume": "56743100"}, {"datetime": "2023-10-11", "open": "178.20000", "high": "179.85001", "low": "177.60001", "close": "179.80000", "volume": "47551100"}, {"datetime": "2023-10-10", "open": "178.10001", "high": "179.72000", "low": "177.95000", "close": "178.39000", "volume": "43698000"}, {"datetime": "2023-10-09", "open": "176.81000", "high": "179.05000", "low": "175.80000", "close": "178.99001", "volume": "42390800"}], "status": "ok"}'
     # obj = json.loads(json_dump)
-
     # data = {'date':[], 'close':[], 'volume':[]}
     # for val in obj['values']:
     #     data['date'].insert(0,datetime.datetime.strptime(val['datetime'], '%Y-%m-%d'))
     #     data['close'].insert(0,float(val['close']))
     #     data['volume'].insert(0,float(val['volume']))
-
     # GET DATAFRAME FROM JSON
     # dataframe = pd.DataFrame(data=data)
+
+
     # GET DATAFRAME FROM CSV
-    dataframe = pd.read_csv('output100.csv')
-    dataframe = dataframe.set_index('date')
+    dataframe = pd.read_csv('aapl1000output.csv')
+    # dataframe = dataframe.set_index('date')
 
     dataframe = create_labels(dataframe)
     dataframe = calculate_bollinger_bands(dataframe)
     dataframe = calculate_obv(dataframe)
-    # dataframe.to_csv('output.csv')
+    # dataframe.to_csv('aapl1000output.csv')
 
     clf = RandomForestClassifier(random_state=0)
     # read from 22 onwards to ignore the initial data where moving average has yet to form
